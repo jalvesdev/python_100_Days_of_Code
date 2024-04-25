879d35337a2f1b94e56dae5ec52b95197104b884
José Alves <54028819+jalvesdev@users.noreply.github.com>
2024-04-25 19:54:57 +0000

    Day 2: data type, type conversion

--- day_001/main.py
+++ day_001/main.py
@@ -1,10 +1,10 @@
 #1. Create a greeting for your program.
-#2. Ask the user for the city that they grew up in.
-#3. ask the user for the name of a pet.
-#4. Combine the name of their city and pet and show them their band name.
-#5. Make sure the input cursor shows on a new line:
-
 print("Welcome to the Band Name Generator.")
+#2. Ask the user for the city that they grew up in.
 city = input("What's the name of the city you grew up in?\n")
+#3. ask the user for the name of a pet.
 pet = input("What's your pet's name?\n")
-print("Your band name could be", city, pet)
+#4. Combine the name of their city and pet and show them their band name.
+response = city + " " + pet
+#5. Make sure the input cursor shows on a new line:
+print("Your band name could be", response)
---
+++ day_002/main.py
@@ -0,0 +1,13 @@
+# type conversion
+
+num_char = len(input("what is your name? "))
+
+print(type(num_char))
+
+new_num_char = str(num_char)
+
+print("your name has " + new_num_char + " characters.")
+
+
+a = 123
+print(type(a))
